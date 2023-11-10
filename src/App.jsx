1318e0b4d9ace318a9a77b520cbf08f7740db580
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-10 17:14:12 +0530

    deleted:    public/vite.svg 	modified:   src/App.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/Search.jsx
    making homepag layout and adding initial nabvar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 function App() {
   return (
     <>
-      <h1 className="text-center">ExploreX</h1>
+      <Navbar />
+      <h1 className="text-center text-2xl mt-52">ExploreX</h1>
+      <Search />
     </>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+const Search = () => {
+  return (
+    <div className="text-center mt-4">
+      <input
+        type="text"
+        value=""
+        placeholder="Start typing..."
+        className="border w-1/2 rounded-3xl px-4 py-2 border-slate-500 hover:shadow-lg tracking-wide"
+      ></input>
+    </div>
+  );
+};
+
+export default Search;
