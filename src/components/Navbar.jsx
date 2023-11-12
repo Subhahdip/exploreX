43056ef184d8581f870ae3f71d68114e6ae4c51a
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-12 17:33:39 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Navbar.jsx navbar UI complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,38 @@
 import { Link } from "react-router-dom";
-
+import AppsIcon from "@mui/icons-material/Apps";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 const Navbar = () => {
   return (
-    <div className="flex align-middle px-2 py-4 justify-between">
+    <div className="flex bg-gray-50 align-middle px-6 py-4 justify-between">
       <div className="">
-        <Link to="/about">About</Link>
-        <Link to="/store">Store</Link>
+        <Link
+          to="/about"
+          className="mr-4 text-sm text-gray-500 hover:underline"
+        >
+          About
+        </Link>
+        <Link
+          to="/store"
+          className="mr-4 text-sm text-gray-500 hover:underline"
+        >
+          Store
+        </Link>
       </div>
       <div>
-        <Link to="/gmail">Gmail</Link>
-        <Link to="/images">Images</Link>
+        <Link
+          to="/gmail"
+          className="mr-4 text-sm text-gray-500 hover:underline"
+        >
+          Gmail
+        </Link>
+        <Link
+          to="/images"
+          className="mr-4 text-sm text-gray-500 hover:underline"
+        >
+          Images
+        </Link>
+        <AppsIcon className="mr-4" />
+        <AccountCircleIcon />
       </div>
     </div>
   );
