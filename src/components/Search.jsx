26fc2a4548ff8fa7898835e9dd99077d9393f70c
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-13 13:49:42 +0530

    modified:   src/components/Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,16 @@
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
+
 const Search = () => {
   return (
-    <div className="text-center mt-4">
+    <div className="text-center mt-4 ">
+      <SearchIcon className="" />
       <input
         type="text"
-        value=""
         placeholder="Start typing..."
-        className="border w-1/2 rounded-3xl px-4 py-2 border-slate-500 hover:shadow-lg tracking-wide"
+        className="border w-1/2 rounded-3xl px-14 py-2 border-slate-500 hover:shadow-lg tracking-wide"
       ></input>
+      <MicIcon />
     </div>
   );
 };
