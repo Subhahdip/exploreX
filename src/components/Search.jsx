517c65ed723bfac2ad591c0ecb6eaf7f967b450e
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-14 17:30:23 +0530

    modified:   src/components/Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,18 @@
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
+import { useState } from "react";
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="flex items-center mt-4 border rounded-3xl border-slate-500 hover:shadow-lg px-5 my-0 mx-auto max-w-md">
       <SearchIcon className="mr-2" />
       <input
         type="text"
-        placeholder="Start typing..."
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+        placeholder="Start Typing..."
         className="flex-grow focus:outline-none py-2"
       />
       <MicIcon className="ml-2" />
