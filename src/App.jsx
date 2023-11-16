9449cdf6dabc2f85fe5512a361d9e32ba92350cb
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-16 16:55:17 +0530

    modified:   src/App.jsx added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
+// import SearchResults from "./components/SearchResults"; // Import the SearchResults component
 
 function App() {
   return (
     <Router>
-      <>
-        <Navbar />
-        <h1 className="text-center text-2xl mt-52">ExploreX</h1>
-        <Search />
-        {/* <Footer /> */}
-      </>
+      <Routes>
+        {/* Route for /search page */}
+        <Route
+          path="/search"
+          element={<h1>This is the searchResult page</h1>}
+        />
+
+        {/* Default route for the homepage */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <h1 className="text-center text-2xl mt-52">ExploreX</h1>
+              <Search />
+              {/* <Footer /> */}
+            </>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
