2ecdd8e1ff6faabc1ae56a533b8c2225c024afda
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-18 21:01:48 +0530

    modified:   src/useGoogleSearch.js creasted custom hook for fetching data

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -3,8 +3,23 @@ import API_KEY from "./keys";
 
 const searchEngineId = "76ad510acf79a4a2c";
 
-const useGoogleSearch = () => {
-  return <div>useGoogleSearch</div>;
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(`https://www.googleapis.com/customsearch/v1?
+      key=${API_KEY}&cx=${searchEngineId}&q=${term}
+      `)
+        .then((response) => response.json)
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+
+  return data;
 };
 
 export default useGoogleSearch;
