4b65afc04c2fe02a072400d13c93205c41728a52
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-19 13:09:33 +0530

    modified:   src/components/SearchResults.jsx getting the results design ready

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -14,6 +14,27 @@ const SearchResults = () => {
         About {mockResponse.searchInformation.formattedTotalResults} results in{" "}
         {mockResponse.searchInformation.formattedSearchTime} seconds for {term}{" "}
       </p>
+      <div className="mt-8">
+        {mockResponse.items.map((item) => (
+          <div key={item.link} className="mb-6">
+            <a
+              href={item.link}
+              className="text-green-700 text-sm hover:underline"
+            >
+              {item.link}
+            </a>
+            <br />
+            <a
+              href={item.link}
+              className="text-blue-700 text-xl font-medium hover:underline"
+            >
+              {item.title}
+            </a>
+
+            <p className="text-gray-700 mt-2">{item.snippet}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
